fbb442126532ff58f042abb08879e0cb207a6c2b
Vincent <vincentnewkirk@gmail.com>
2018-08-25 14:24:35 +0900

    Import String Method Length

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -2,6 +2,7 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String exposing (length)
 
 
 
@@ -72,7 +73,9 @@ viewInput t p v toMsg =
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
+  if length model.password > 8 then
+    div [style "color" "red" ] [ text "Password Too Long" ]
+  else if model.password == model.passwordAgain then
     div [ style "color" "green" ] [ text "OK" ]
   else
     div [ style "color" "red" ] [ text "Passwords do not match!" ]
